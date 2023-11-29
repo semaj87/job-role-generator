2d72cd8446f1c5cf97b3d7aced2a06cf1a584b13
James Aymer <aymerjames@gmail.com>
2023-11-29 00:17:29 +0000

    WIP: Refactoring app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import requests
 
 from dotenv import find_dotenv, load_dotenv
+from linkedin_api import Linkedin
+from typing import Any
 
 
 # ---------loading credentials--------- #
@@ -9,70 +11,41 @@ load_dotenv(find_dotenv())
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 HUGGINGFACE_API_TOKEN = os.getenv("HUGGINGFACE_API_TOKEN")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
+LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME")
+
+
+# ---------linkedin client--------- #
+linkedin_client: Any = Linkedin(LINKEDIN_USERNAME, LINKEDIN_PASSWORD)
 
 
 # ------------------getting user information via the LinkedIn API------------------ #
-def generate_authorisation_url():
+def get_linkedin_profile(client: Any, username: str) -> dict[str, str]:
     """
-    Generate an authorization URL for a user to give permission to extract their LinkedIn Profile.
-
-    The genereated URL will take the user to a LinkedIn page where the user will be asked to give explicit
-    permission to share their profile with you (the application creator).
-
-    Should the user agree, they will be redirected to `LINKEDIN_REDIRECT_URI`.
-    In the redirect, two fields will appear in the URL parameter, namely `code` and `state`.
-
-    * `state` is generated below using `secrets.token_hex(8).upper()`. This is as a form of identifier for this user.
-    * `code` is the authorization_code, and can be used in `get_access_token()` to exchange for an `access_token`.
-    :return None
+    This function is used to get the profile of the specified user
+    :param client: LinkedIn client
+    :param username: LinkedIn username
+    :return: user profile dict
     """
-    LI_AUTH_URL = 'https://www.linkedin.com/oauth/v2/authorization'
-    url = requests.Request('GET', LI_AUTH_URL,
-                           params={
-                               'response_type': 'code',
-                               'client_id': LINKEDIN_CLIENT_ID,
-                               'redirect_uri': LINKEDIN_REDIRECT_URI,
-                               'state': secrets.token_hex(8).upper(),
-                               'scope': '%20'.join(['r_liteprofile', 'r_emailaddress', 'w_member_social']),
-                           }).prepare().url
-    return url
+    profile: dict = client.get_profile(username)
+    print(profile)
 
-
-def get_access_token(authorisation_code: str):
-    """
-    Given a authorization `code`, this function will return you `access_token` which can then be used to access a user's
-     LinkedIn profile.
-     :param authorisation_code: generated code from user permission
-    """
-    LI_ACCESS_TOKEN_EXCHANGE_URL = 'https://www.linkedin.com/oauth/v2/accessToken'
-    access_token = requests.post(LI_ACCESS_TOKEN_EXCHANGE_URL, params={
-        'grant_type': 'authorization_code',
-        'code': authorisation_code,
-        'redirect_uri': LINKEDIN_REDIRECT_URI,
-        'client_id': LINKEDIN_CLIENT_ID,
-        'client_secret': LINKEDIN_CLIENT_SECRET,
-    }).json()['access_token']
-
-    return access_token
-
-
-def get_user_profile(access_token: str) -> str:
-    """
-    This function is used to access a user's LinkedIn profile and retrieve the data
-    :param access_token: token to access the user's profile
-    :return JSON representation of the user's profile
-    """
-    LI_PROFILE_API_ENDPOINT = 'https://api.linkedin.com/v2/me'
-    r = requests.get(LI_PROFILE_API_ENDPOINT, headers={
-    'Authorization': 'Bearer ' + access_token})
-
-    return r.json()
+    return profile
 
 
 # ------------------find the most relevant keywords & data to improve the search------------------ #
-def find_job_specific_data_from_profile(response_data: dict) -> list:
+def find_job_related_keywords(linkedin_profile_dict: dict) -> list[str]:
+    """
+    This function is used to retrieve all the job related keywords from a user profile
+    :param linkedin_profile_dict:
+    :return: list of job related keywords
     """
 
-    :param response_data:
-    :return:
+
+# ------------------using serpapi to search for jobs using keywords------------------ #
+def serp_search_for_jobs(query: str) -> dict:
+    """
+    This function is used to search for jobs using the serpapi
+    :param query: query used to search
+    :return: dict of search results
     """
